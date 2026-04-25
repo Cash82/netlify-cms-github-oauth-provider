35dc3544aa8e779e98b420c034988d4ba729aaae
Cash82 <106923433+Cash82@users.noreply.github.com>
2026-04-25 14:19:16 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,27 +1,21 @@
 const { AuthorizationCode } = require('simple-oauth2');
 
-module.exports = {
-  getClient: () => {
-    const config = {
-      client: {
-        id: process.env.OAUTH_CLIENT_ID,
-        secret: process.env.OAUTH_CLIENT_SECRET
-      },
-      auth: {
-        tokenHost: process.env.GIT_HOSTNAME || 'https://github.com',
-        tokenPath: '/login/oauth/access_token',
-        authorizePath: '/login/oauth/authorize'
-      }
-    };
-    return new AuthorizationCode(config);
+const config = {
+  client: {
+    id: process.env.OAUTH_CLIENT_ID,
+    secret: process.env.OAUTH_CLIENT_SECRET
   },
-
-  getToken: async (code) => {
-    const client = module.exports.getClient();
-    const tokenParams = {
-      code,
-      redirect_uri: process.env.REDIRECT_URL
-    };
-    return await client.authorizationCode.getToken(tokenParams);
+  auth: {
+    tokenHost: process.env.GIT_HOSTNAME || 'https://github.com',
+    tokenPath: '/login/oauth/access_token',
+    authorizePath: '/login/oauth/authorize'
   }
 };
+
+const oauth2 = new AuthorizationCode(config);
+
+const authMiddleWareInit = (provider) => {
+  return oauth2;
+};
+
+module.exports = authMiddleWareInit;
